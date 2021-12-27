e7e036272cd55f691788689c8376c08dfc981c13
Abdus Samee <abdussamee16@gmail.com>
2021-12-28 01:30:50 +0600

    Added testing route for streaming

--- routes/oracle.js
+++ routes/oracle.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var oracledb = require('oracledb')
+var fs = require('fs')
 
 //to connect to64-bit oracle client --> gives error if not connected
 if (process.platform === 'win32') {
@@ -44,5 +45,48 @@ router.get('/', (req, res, next) => {
         }
     })
 })
+
+router.get("/video", function (req, res) {
+  // Ensure there is a range given for the video
+  const range = req.headers.range
+  // get video stats (about 61MB)
+  const videoPath = "public/videos/102.mp4"
+  const videoSize = fs.statSync("public/videos/102.mp4").size
+  console.log(videoPath)
+  console.log(videoSize)
+
+  if (!range) {
+    const head = {
+        'Content-Length': videoSize,
+        'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(videoPath).pipe(res)
+  }else{
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6 // 1MB
+    const start = Number(range.replace(/\D/g, ""))
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+
+    // Create headers
+    const contentLength = end - start + 1
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    }
+
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers)
+
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end })
+
+    // Stream the video chunk to the client
+    videoStream.pipe(res)
+  }
+});
 
 module.exports = router
